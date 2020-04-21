7ec98cc91ff8c5eac36107e0b57281be56c96e62
Stiliyan Dragnev <stiliyan.dragnev@gmail.com>
2020-04-21 23:52:30 +0300

    Modify the puzzle program

--- 8-puzzle/puzzle.py
+++ 8-puzzle/puzzle.py
@@ -4,9 +4,40 @@ from eightpuzzle.astar.search import AStarSearch
 from eightpuzzle.state import State
 
 
-def find_solution(initial_state_path, goal_path):
-    initial_state = State.from_CSV(initial_state_path)
-    goal = State.from_CSV(goal_path)
+DEFAULT_GOAL = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", " "],
+]
+
+
+def determine_initial_and_goal_states(
+    command_line_arguments
+):
+    if (command_line_arguments):
+        initial_state_path, goal_path = \
+            command_line_arguments
+
+        return (State.from_CSV(initial_state_path),
+                State.from_CSV(goal_path))
+    else:
+        return (State.random(),
+                State.from_list(DEFAULT_GOAL))
+
+
+def log_error(message):
+    sys.stderr.write(f"{message}\n")
+
+
+def print_states(initial, goal):
+    print("Initial state:",
+          initial,
+          "Goal state:",
+          goal,
+          sep="\n")
+
+
+def find_solution(initial_state, goal):
     search = AStarSearch()
 
     return search(initial_state, goal)
@@ -16,10 +47,11 @@ def print_solution(s):
     if (s is not None):
         _do_print_solution(s)
     else:
-        print("There is no solution!")
+        print("No solution")
 
 
 def _do_print_solution(s):
+    print("Solution:")
     rest = _print_initial_state_from(s)
 
     for action, state in rest:
@@ -42,22 +74,24 @@ def _print_action_generated_state(action, state):
           sep="\n")
 
 
-def log_error(message):
-    sys.stderr.write(f"{message}\n")
-
-
 if (__name__ == "__main__"):
-    if (len(sys.argv) == 3):
+    command_line_args = sys.argv
+    number_of_arguments = len(command_line_args)
+
+    if (number_of_arguments in [1, 3]):
         try:
-            s = find_solution(
-                initial_state_path=sys.argv[1],
-                goal_path=sys.argv[2]
-            )
+            initial_state, goal = \
+                determine_initial_and_goal_states(
+                    command_line_args[1:]
+                )
         except Exception as e:
             log_error(str(e))
         else:
+            print_states(initial_state, goal)
+            s = find_solution(initial_state, goal)
             print_solution(s)
     else:
         log_error(
-            "Expected paths for initial and goal states!"
+            "Invalid number of command line "
+            f"arguments for {command_line_args[0]!r}!"
         )
